8889a5adbc4581838665d8105b12df5c5909b06b
Nathankwon <30610639+nathankwon@users.noreply.github.com>
2019-02-11 20:22:51 +1100

    first commit

---
+++ src/components/ClearButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ClearButton.css';
+
+export const ClearButton = (props) => {
+  return <div className="clear-btn" onClick={props.handleClear}>
+      {props.children}
+    </div>
+}
